96716f92a00726e74ccc15af37a6655945f38cee
@蒲小帅 <1372553910@qq.com>
2018-11-20 21:49:17 +0800

    Update top100.py

--- top100.py
+++ top100.py
@@ -3,22 +3,29 @@
 # @Author  : Puhao
 # @File    : spider.py
 # @Software: PyCharm
-#===========================������Ӧ�Ŀ�.è�۵�Ӱtop100
+
+from multiprocessing.pool import Pool
+
 import requests
-import urllib.request
-import re
-from multiprocessing import Pool
-import json
 from requests.exceptions import RequestException
-#=========================��������
+import re
+import json
+
+
 def get_onepage(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    }
     try:
-        respose = urllib.request.urlopen(url)
-        respo = respose.read().decode('utf-8')
-        return respo
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print(response.status_code)
     except RequestException:
         return None
-#=========================������
+
+
 def main(page):
     url = "http://maoyan.com/board/4?offset=" + str(page)
     html = get_onepage(url)
@@ -26,13 +33,7 @@ def main(page):
         print(item)
         write_txt(item)
 
-'''re.I�����Դ�Сд
-re.L����ʾ�����ַ��� \w, \W, \b, \B, \s, \S �����ڵ�ǰ����
-re.M������ģʽ
-re.S��' . '���Ұ������з����ڵ������ַ���ע�⣺' . '���������з��� .html������
-re.U�� ��ʾ�����ַ��� \w, \W, \b, \B, \d, \D, \s, \S ������ Unicode �ַ��������ݿ�'''
-# һ�·ֲ�ƥ�� ������ͼƬ �����⣬���ݣ���ӳʱ�䣬����
-#=====================����ƥ��
+
 def parse_onepge(html):
     pattern = re.compile(
         '<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?title="(.*?)".*?star">(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>',
@@ -43,18 +44,21 @@ def parse_onepge(html):
             'index': item[0],
             'image': item[1],
             'title': item[2],
-            'actor': item[3],
-            'time': item[4],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
             'score': item[5] + item[6]
         }
-#=====================д���ļ�
+
+
 def write_txt(content):
     with open('result.txt', 'a', encoding='utf-8') as f:
         f.write(json.dumps(content, ensure_ascii=False) + "\n")
         f.close()
-#============�̳߳�ִ��
+
+
 if __name__ == '__main__':
     pool = Pool()
     pool.map(main, [i * 10 for i in range(10)])
     pool.close()
     pool.join()
+
