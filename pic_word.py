9d01952268df2ef30695c9d58f09785430a45ae6
puxiaoshuai <1372553910@qq.com>
2018-11-20 17:07:13 +0800

    根据背景图的颜色，做文字的颜色

--- pic_word.py
+++ pic_word.py
@@ -1,5 +1,6 @@
 # 生成图云
 from random import random
+from scipy.misc import imread  # 获取颜色
 import matplotlib.pyplot as plt
 import jieba  # 辅助库，把不必要的词语剔除
 from wordcloud import WordCloud, ImageColorGenerator
@@ -18,15 +19,20 @@ text = text.replace(u"元首说", u"元首")
 cut_text = jieba.cut(text)
 reslt = '/'.join(cut_text)  # 必须用符号分开
 # 初始化背景图
-image = Image.open("D:\Python_project\PythonPractice\mask1.png")
-graph = np.array(image)
-
+# image = Image.open("D:\Python_project\PythonPractice\mask.jpg")
+# graph = np.array(image)
+graph = imread(r"D:\Python_project\PythonPractice\mask.jpg")
+# 获取图片的颜色作为文字颜色
+image_colors = ImageColorGenerator(graph)
 # 生成词云图，这里需要注意的是WordCloud默认不支持中文，所以这里需已下载好的中文字库
-wc = WordCloud(font_path=r"D:\Python_project\PythonPractice\msyh.ttf", width=600, height=600, max_font_size=64
-               , max_words=300, min_font_size=18, background_color='white', mask=graph, mode='RGBA')
+wc = WordCloud(font_path=r"D:\Python_project\PythonPractice\msyh.ttf", width=600, height=600, margin=10,
+               max_font_size=64
+               , max_words=300, color_func=image_colors, min_font_size=18, background_color='white', mask=graph,
+               mode='RGBA')
 wc.generate(reslt)
 
 wc.to_file("D:\Python_project\PythonPractice\wordcloud.png")
+
 # 显示图片
 plt.figure()
 plt.title("词云图")  # 指定名称
